c466f64cea20e18451dc52446c95c61e98273211
Fredrik Laaksonen <fredrik.laaksonen@gmail.com>
2026-09-02 16:22:27 +0200

    Lade till bilder

---
+++ bilder/portfolio.js
@@ -0,0 +1,9 @@
+var PORTFOLIO_ITEMS = [
+  {
+    "id": "p1",
+    "title": "Formgivning av hammarhajsdusch till Göteborgs stad ",
+    "ingress": "Utöver produktionen av träprototyper erbjuder vi 3D-modelering, om tid finns.",
+    "body": "Hammarhajen skapades i Blender och frästes sedan till en frigolitkärna med en sex-axlig ABB robot. Den plastades, lackades och duschassemblaget monteradses innan den fraktades till barnen i Hammarkullen. ",
+    "image": "p1.jpg"
+  }
+];
